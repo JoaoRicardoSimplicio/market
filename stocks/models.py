d879557cd2d7ad281622501d7819ba4286c6228b
João Ricardo Simplício <joaoricardosimplicio16@gmail.com>
2020-12-22 16:20:50 -0300

    Refactoring the stock table and creating a table to buy and sell stocks

--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
@@ -7,13 +9,25 @@ class Stock(models.Model):
 
     code = models.CharField(unique=True, max_length=10)
     amount = models.IntegerField(null=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    highest_price_day = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    lowest_price_day = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    day_variation = models.DecimalField(max_digits=6, decimal_places=2, null=True)
-    month_variation = models.DecimalField(max_digits=6, decimal_places=2, null=True)
-    year_variation = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     description = models.TextField(null=True)
 
     def __str__(self):
         return self.code
+
+
+class StockBuy(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name='buys')
+    amount = models.IntegerField(null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    date = models.DateField(auto_now_add=True)
+
+
+class StockSale(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name='sales')
+    amount = models.IntegerField(null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+    date = models.DateField(auto_now_add=True)
